6ce0f81819c93e64c89db20e62566f87e4d0796b
student <student@ad.local>
2021-08-19 12:08:08 +0300

    update table katalog in database Sale

--- sql/newmodel.sql
+++ sql/newmodel.sql
@@ -1,14 +1,20 @@
 CREATE DATABASE Sale;
-
+/*
+создается база данныйх Sale
+*/
 USE Sale;
+
+/*
+таблица базы данных продаж каталог
+*/
 CREATE TABLE katalog (
-    model INTEGER AUTO_INCREMENT PRIMARY KEY, 
-    denomination VARCHAR(30), 
-    color varchar(11),
-    height integer,
-    width integer,
-    volume integer,
-    freezer_volume integer,
-    refrigerating_chamber_volume integer,
-    depth integer
+    model INTEGER AUTO_INCREMENT PRIMARY KEY,  # номер модели
+    denomination VARCHAR(30), # наименование
+    color varchar(11), # цвет
+    height integer, # высота
+    width integer, # ширина
+    volume integer, # объем
+    freezer_volume integer, # объем морозной камеры
+    refrigerating_chamber_volume integer, # объем холодильной камеры
+    depth integer # глубина
 );
