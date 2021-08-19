aca33c4798f1f1a2fb63d7c89ae40362d6f96dd2
student <student@ad.local>
2021-08-19 13:25:50 +0300

    update newmodel

--- sql/newmodel.sql
+++ sql/newmodel.sql
@@ -18,3 +18,32 @@ CREATE TABLE models (
     refrigerating_chamber_volume decimal(4, 2), # объем холодильной камеры
     depth decimal(4, 2) # глубина
 );
+
+create table Product ( /*ñîçäàíèå òàáëèöû Product*/
+	ItemNumber int auto_increment primary key, /*íîìåð òîâàðà - ïåðâè÷íûé êëþ÷ òàáëèöû Product*/
+	FOREIGN KEY(CategoryNumber) REFERENCES  Category (CategoryNumber), /*íîìåð êàòåãîðèè - âòîðè÷íûé êëþ÷ òàáëèöû Product*/
+	FOREIGN KEY(ModelNumber) REFERENCES  Model (ModelNumber), /*íîìåð ìîäåëè - âòîðè÷íûé êëþ÷ òàáëèöû Product*/
+	Price float /*öåíà òîâàðà*/
+	);
+
+ create table AdditionalServices ( /*ñîçäàíèå òàáëèöû AdditionalServices*/
+	ServiceNumber int auto_increment primary key, /*íîìåð óñëóãè - ïåðâè÷íûé êëþ÷ òàáëèöû AdditionalServices*/
+	NameOfService varchar (30), /*íàçâàíèå óñëóãè*/
+	Price float /*öåíà òîâàðà*/
+	);
+    
+    CREATE TABLE catalog (
+    product INTEGER AUTO_INCREMENT PRIMARY KEY,  # номер товара
+    presence TINYBLOB, # присутствие
+    descr TEXT, # описание
+    FOREIGN KEY(ServiceName) REFERENCES AdditionalServices(ServiceNumber), # номер услуги
+    final_price DECIMAL(10,2) # итоговая цена
+);
+
+/*
+таблица базы данных продаж категория
+*/
+CREATE TABLE category (
+    categ INTEGER AUTO_INCREMENT PRIMARY KEY,  # номер товара
+    namae TINYTEXT # наименование
+);
