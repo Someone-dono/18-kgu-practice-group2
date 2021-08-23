46719197fb64dd1b1a57990158c5c586434ceeb8
Kruuliter <solenit@mail.ru>
2021-08-23 13:48:13 +0300

    services

---
+++ hibernatetest/src/main/java/ru/javabegin/training/hibernate/entity/Models.java
@@ -0,0 +1,29 @@
+package ru.javabegin.training.hibernate.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+
+public class Models implements Serializable {
+    @Id
+    @GeneratedValue(stradegy = GenerationType.IDENTITY)
+    private long model;
+    private String denomination;
+    private String color;
+    private Integer height;
+    private Integer width;
+    private Double volume;
+    private Double freezer_volume;
+    private Double refrigerating_chamber_volume;
+    private Double depth;
+}
---
+++ hibernatetest/src/main/java/ru/javabegin/training/hibernate/entity/category.java
@@ -0,0 +1,4 @@
+package ru.javabegin.training.hibernate.entity;
+
+public class category {
+}
