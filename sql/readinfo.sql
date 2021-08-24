cfc0d595ce7736851926e34a82ff5551b3607fdf
Kruuliter <solenit@mail.ru>
2021-08-24 12:36:44 +0300

    update sql

--- sql/readinfo.sql
+++ sql/readinfo.sql
@@ -2,3 +2,6 @@ use sale;
 
 select * from category;
 select * from AdditionalServices;
+select * from Models;
+select * from Product;
+select * from Catalog;
--- sql/writeinfo.sql
+++ sql/writeinfo.sql
@@ -1,5 +1,13 @@
 use sale;
 
-insert into AdditionalServices(NameOfService, Price) value ('first', 15.5);
-insert into AdditionalServices(NameOfService, Price) value ('second', 20);
-insert into AdditionalServices(NameOfService, Price) value ('third', 17.3);
+#insert into Models(denomination, color, height, width, volume, freezer_volume, refrigerating_chamber_volume, depth) value ("Hiber one", "red", 5, 1, 9.5, 2.8, 0.5, 0.01);
+
+#insert into Product(CategoryNumber, modelNumber, Price) value(1, 3, 25000);
+#insert into Product(CategoryNumber, modelNumber, Price) value(2, 1, 21000);
+#insert into Product(CategoryNumber, modelNumber, Price) value(3, 2, 12000);
+#insert into Product(CategoryNumber, modelNumber, Price) value(4, 1, 17000);
+
+#insert into Catalog(ItemNumber, presence, descr, ServiceNumb, final_price) value(1, "yes", "существует", 3, 28000);
+#insert into Catalog(ItemNumber, presence, descr, ServiceNumb, final_price) value(4, "no", "не существует", 1, 30000);
+#insert into Catalog(ItemNumber, presence, descr, ServiceNumb, final_price) value(3, "yes", "существует, но не тут", 2, 1000000);
+#insert into Catalog(ItemNumber, presence, descr, ServiceNumb, final_price) value(2, "yes", "существует, но дешевый", 3, 18000);
